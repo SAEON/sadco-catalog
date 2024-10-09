83f19bf380281434c28d5f023da36620ee6ae27d
dylan <d.pivo@saeon.nrf.ac.za>
2024-10-09 15:01:18 +0200

    improved downloads ui and paging

--- sadco/ui/catalog/views/download_audit.py
+++ sadco/ui/catalog/views/download_audit.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from sadco.const import SADCOScope
 from odp.ui.base import api
 
@@ -8,10 +8,12 @@ bp = Blueprint('download_audit', __name__, static_folder='../static')
 @bp.route('/', methods=('GET',))
 @api.view(SADCOScope.DOWNLOAD_READ)
 def index():
-    downloads = api.get('/downloads/my_downloads')
+    page = request.args.get('page', 1)
+    downloads = api.get('/downloads/my_downloads', page=page)
 
     return render_template(
         'downloads.html',
+        all_downloads_required_scope=SADCOScope.DOWNLOAD_ADMIN,
         downloads_type='my',
         result=downloads
     )
@@ -20,10 +22,12 @@ def index():
 @bp.route('/all_downloads', methods=('GET',))
 @api.view(SADCOScope.DOWNLOAD_ADMIN)
 def all_downloads():
-    downloads = api.get('/downloads/all_downloads')
+    page = request.args.get('page', 1)
+    downloads = api.get('/downloads/all_downloads', page=page)
 
     return render_template(
         'downloads.html',
+        all_downloads_required_scope=SADCOScope.DOWNLOAD_ADMIN,
         downloads_type='all',
         result=downloads,
     )
